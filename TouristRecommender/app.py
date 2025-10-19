53dd0de00a83812939ad7d0877f7f4c0e1c4cf73
Shambhuraje Jadhav <168363534+Shambhujadhav4@users.noreply.github.com>
2025-10-19 14:43:07 +0530

    Path change

--- TouristRecommender/app.py
+++ TouristRecommender/app.py
@@ -176,7 +176,7 @@ def load_data():
     # try loading dataset from expected path
     try:
         df = pd.read_csv("TouristRecommender/Touristnew.csv")
-        d = pd.read_csv("Tourist5.csv")
+        d = pd.read_csv("TouristRecommender/Tourist5.csv")
     except Exception as e:
         # If failing, return None to let UI show fallback message
         return None, None
